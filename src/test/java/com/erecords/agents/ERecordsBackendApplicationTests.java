aaf6b73e6bc204fbd57d0872abf1069210d1917b
JonelSV <svjonel@gmail.com>
2021-10-19 20:39:11 -0400

    first commit

---
+++ src/test/java/com/erecords/agents/ERecordsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erecords.agents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERecordsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
